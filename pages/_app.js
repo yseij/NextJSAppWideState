50e94b65b088dbb634a2b2da2b19139c1aed6a8b
yseij <r0790156@student.thomasmore.be>
2022-02-07 16:08:55 +0100

    Nieuw React Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,26 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="this is a test" status="success" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
